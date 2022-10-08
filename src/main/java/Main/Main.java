aeca921fb928301a1b0aba0e43bb35d8dd90bb74
Maksim Somsin <msomsin@mail.ru>
2022-10-08 11:17:11 +0300

    2

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,18 +1,50 @@
 package Main;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main( String[] args )
     {
-
+        // Создание каркаса
         JFrame frame = new JFrame();
         frame.setSize(900, 600);
-
         frame.add(new MainForm().getMainPanel());
-
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        // Создание панели меню и добавление компонентов
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("FILE");
+        JMenu m2 = new JMenu("Help");
+        mb.add(m1);
+        mb.add(m2);
+        JMenuItem m11 = new JMenuItem("Открыть");
+        JMenuItem m22 = new JMenuItem("Сохранить как");
+        m1.add(m11);
+        m1.add(m22);
+
+        // Создание панели внизу и добавление компонентов
+        JPanel panel = new JPanel(); // панель не видна при выводе
+        JLabel label = new JLabel("Введите текст");
+        JTextField tf = new JTextField(10); // принимает до 10 символов
+        JButton send = new JButton("Отправить");
+        JButton reset = new JButton("Сброс");
+        panel.add(label); // Компоненты, добавленные с помощью макета Flow Layout
+        panel.add(tf);
+        panel.add(send);
+        panel.add(reset);
+
+        // Текстовая область по центру
+        JTextArea ta = new JTextArea();
+
+        // Добавление компонентов
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.setVisible(true);
+
+
     }
 }
